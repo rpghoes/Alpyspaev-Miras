9b20e4efb307f764731733145d3b7546502c53fe
Miras <rpghoes@gmail.com>
2025-06-23 15:54:57 +0500

    задание от 10 до 20

---
+++ Exercise-11/app/tasks.py
@@ -0,0 +1,9 @@
+﻿import time
+from app.celery_app import celery
+
+@celery.task
+def send_email(email: str):
+    print(f"📨 Sending email to {email}")
+    time.sleep(5)
+    print(f"✅ Email sent to {email}")
+    return f"Email to {email} sent successfully"
